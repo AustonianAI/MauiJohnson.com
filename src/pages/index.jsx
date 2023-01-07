cf61d594ddc8f64899f59d6a8bc543ee23260a5e
Austin Johnson <austin@austinjohnson.me>
2023-01-07 15:34:45 -0600

    basic favicon, title, avatar

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -250,24 +250,21 @@ export default function Home({ articles }) {
   return (
     <>
       <Head>
-        <title>
-          Spencer Sharp - Software designer, founder, and amateur astronaut
-        </title>
+        <title>Maui Johnson - Interior Designer based ATX</title>
         <meta
           name="description"
-          content="I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms."
+          content="I’m Maui, an interior designer in Austin, TX. With director level experience for top interior design and architecture firms, I am a thorough professional with a passion for design."
         />
       </Head>
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software designer, founder, and amateur astronaut.
+            Maui Johnson - Interior Designer based in ATX
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            I’m Maui, an interior designer in Austin, TX. With director level
+            experience for top interior design and architecture firms, I am a
+            thorough professional with a passion for design.
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
